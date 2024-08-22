9813f3f2fe4fecd530de4704bfb649811c9ece8a
Doulo Daff <doulinf.daff91@gmail.com>
2024-08-22 21:18:37 +0000

    Folder structure setup

---
+++ app/(auth)/(routes)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SignIn() {
+  return (
+    <p>This is an unprotected page.</p>
+  )
+}
+
+export default SignIn
---
+++ app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,7 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <p>This is a Protected Page.</p>
+  );
+}
